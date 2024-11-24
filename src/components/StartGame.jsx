056d6e2c0b474ccee6d23f00be372532bc2cc317
Farhan <farhan06.contact@gmail.com>
2024-11-24 12:52:43 +0600

    intro

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -2,7 +2,7 @@ export default function StartGame(props){
     return(
         <section id="start-game">
             <h1>Qwizard</h1>
-            <p>How smart are you? Let's see if you can solve some fun tech quizzes!</p>
+            <p>How smart are you? Let's see if you can solve some fun quizzes!</p>
             <button className="btn" onClick={props.startQuiz}>Start Quiz</button>
         </section>
     )
